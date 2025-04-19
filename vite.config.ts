2ac35dfc8a2d5423edb542d383319cd46bef070d
victor costantini <penav1058@gmail.com>
2025-04-19 16:52:27 -0400

    feat: devs backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 /// <reference types="vitest" />
 
 import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     legacy()
   ],
   test: {
